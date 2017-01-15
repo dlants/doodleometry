a9dd421824b87fd9942b300e2f72f4f27415bee3
Denis Lantsman <denis@desmos.com>
2017-01-15 07:06:32 -0800

    adding tests for arc construction

--- test/Geometry.purs
+++ test/Geometry.purs
@@ -3,20 +3,32 @@ module Test.Geometry where
 import Prelude
 import App.Geometry
 import Data.List (List(..), (:))
+import Math (pi)
 import Test.Spec (describe, it)
 import Test.Spec.Assertions (shouldEqual)
 
-p1 = Point 0.0 0.0
-p2 = Point 1.0 1.0
-p3 = Point 1.0 0.0
-p4 = Point 0.0 1.0
-
-l12 = Line p1 p2
-l34 = Line p3 p4
-
-
 spec = do
   describe "App.Geometry" do
+    describe "Arc" do
+      it "should be constructed correctly" do
+        (constructArc (Point 1.0 1.0) (Point 2.0 1.0)) `shouldEqual` Arc (Point 1.0 1.0) 1.0 0.0 (2.0 * pi)
+        (constructArc (Point 1.0 1.0) (Point 1.0 2.0)) `shouldEqual` Arc (Point 1.0 1.0) 1.0 (pi / 2.0) (2.0 * pi)
+
+      it "should report back firstPoint correctly" do
+        firstPoint (Arc (Point 1.0 1.0) 1.0 0.0 (2.0 * pi)) `shouldEqual` Point 2.0 1.0
+        firstPoint (Arc (Point 1.0 1.0) 1.0 (pi / 2.0) (2.0 * pi)) `shouldEqual` Point 1.0 2.0
+        firstPoint (Arc (Point 1.0 1.0) 1.0 (-pi / 2.0) (2.0 * pi)) `shouldEqual` Point 1.0 0.0
+        firstPoint (Arc (Point 1.0 1.0) 1.0 pi (2.0 * pi)) `shouldEqual` Point 0.0 1.0
+
+        (firstPoint $ constructArc (Point 10.0 10.0) (Point 20.0 20.0))
+          `shouldEqual` Point 20.0 20.0
+
+      it "should report back secondPoint correctly" do
+        (secondPoint $ constructArc (Point 10.0 10.0) (Point 20.0 20.0))
+          `shouldEqual` Point 20.0 20.0
+
+        secondPoint (Arc (Point 1.0 1.0) 1.0 pi pi) `shouldEqual` Point 2.0 1.0
+
     describe "intersect" do
       it "should find an intersection point" do
         intersect (Line (Point 0.0 0.0) (Point 1.0 1.0)) (Line (Point 0.0 1.0) (Point 1.0 0.0))
